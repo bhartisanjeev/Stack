aa153a06d919b4902f8a7117f11bb22951b1db93
Sanjeev Bharti <bharti.sanjev1@gmail.com>
2015-03-16 22:09:48 -0700

    Stack Implementation
    Implementation of stack using single header link list

---
+++ Stack/Stack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Stack
+//
+//  Created by sanjeev bharti on 3/16/15.
+//  Copyright (c) 2015 sanjeev bharti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
